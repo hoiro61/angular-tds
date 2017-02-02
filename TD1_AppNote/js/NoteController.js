bf315e60d46680194045232d917e0239ae355639
HEBERT Matthias TD1 <21405372@etu.unicaen.fr>
2017-02-03 00:54:36 +0100

    TD1 App Note Cookie et clear fonctionnels
    La note est sauvegardée si on clique sur save
    Un message avertit de la sauvegarde de la note
    On peut effacer la note

--- TD1_AppNote/js/NoteController.js
+++ TD1_AppNote/js/NoteController.js
@@ -1,11 +1,32 @@
-angular.module("AppNote").controller("NoteController", function(){
-	this.messageNote="";
-	var info="";
-	var status=1;
-	this.clear=function(){
-		this.messageNote="";
-	};
-	this.count=function(){
-		return 100-this.messageNote.length;
-	};
-});
+//angular.module("AppNote").controller("NoteController", function(){
+//	var self=this;
+//	this.messageNote="";
+    angular.module("AppNote", ['ngCookies'])
+        .controller("NoteController", ['$cookies', function($cookies) {
+            var self=this;
+        	self.info="";
+            var recupval = $cookies.get("notesav");
+            self.messageNote = recupval || "";
+            var status = 1;
+            self.clear = function () {
+                self.messageNote = "";
+                self.status=0;
+            };
+            self.save = function () {
+                if(self.messageNote != ""){
+                    self.info = "Note sauvegardée";
+                    $cookies.put("notesav",self.messageNote);
+                }
+            };
+            self.count = function () {
+                var calculcount = 100 - self.messageNote.length;
+                if (calculcount<50){
+                    self.status=2;
+                }
+                else if (calculcount<20){
+                    self.status=3;
+                }
+                return calculcount;
+            };
+        }]);
+//});
--- TD1_AppNote/js/mainController.js
+++
@@ -1,6 +0,0 @@
-angular.module("AppNote").controller("MainController", function(){
-	var message="Hello";
-	this.getMsg=function(){
-		return message;
-	};
-});
