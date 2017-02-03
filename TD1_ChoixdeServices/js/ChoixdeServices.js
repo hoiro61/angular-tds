8d9ccfb53ff3a8eb7c679a3a3ffb9bdc65fe1b3d
hoiro <21405372@etu.unicaen>
2017-02-03 12:02:00 +0100

    TD1_ChoixdeServices sélection des services avec calcul du prix et du nombre de services séléectionnés

--- TD1_ChoixdeServices/js/ChoixdeServices.js
+++ TD1_ChoixdeServices/js/ChoixdeServices.js
@@ -1,8 +1,8 @@
     angular.module("AppChoixdeServices")
-        .controller("ChoixdeServices", function() {
+        .controller("ChoixdeServices", ['$http', function(http) {
         	
-            var self=this;
-            this.services =
+            var self = this;
+            self.services =
             	[
             	    {
             	        "name": "Web Development",
@@ -22,4 +22,36 @@
             	        "active":false
             	    }
             	]
-        });
+            
+            self.toggleActive = function (service) {
+            	service.active= !service.active;
+            };
+            
+            self.total = function () {
+            	var compt = 0;
+            	angular.forEach(self.services, function(service, key) {
+            			if (service['active']==true){
+            				compt = compt + service['price'];
+            			}
+            		});
+            	return compt;
+            };
+            
+            self.nbservices = function () {
+            	var compt = 0;
+            	angular.forEach(self.services, function(service, key) {
+            			if (service['active']==true){
+            				compt = compt + 1;
+            			}
+            		});
+            	if (compt==0){
+            		return "Aucun service sélectionné";
+            	}
+            	else if (compt==1){
+            		return compt+" service sélectionné";
+            	}
+            	else {
+            		return compt+" services sélectionnés";
+            	}
+            };
+        }]);
