f38274ced90a868dc472fa4b3e8d2cefbcb83344
hoiro <21405372@etu.unicaen>
2017-02-17 12:01:50 +0100

    TD2_GestiondesContacts Affichage des contacts+affichage du nombre de contacts

--- TD2_GestiondesContacts/js/contactsController.js
+++ TD2_GestiondesContacts/js/contactsController.js
@@ -1,23 +1,55 @@
 angular.module("AppGestiondesContacts").
-controller("contactsController", ['$scope', function($scope){
+controller("contactsController", [ function(){
 	
 	var self = this;
 	
-	$scope.client = {
+	self.clients =
+			[
+			 {
 		    nom: 'SMITH',
-		    prenom: 'John'
-		  };
+		    prenom: 'John',
+		    adresse: '20 rue du test',
+		    ville: 'Ville du test',
+		    cp: '77880',
+		    mail: 'johnsmith@hotmail.fr'
+			 },
+			 {
+		    nom: 'GATES',
+		    prenom: 'Bill',
+		    adresse: '21 rue du test',
+		    ville: 'Ville du test',
+		    cp: '77880',
+		    mail: 'bill@microsoft.com'
+			 },
+			 {
+		    nom: 'JOBS',
+		    prenom: 'Steeve',
+		    adresse: '20 rue du test',
+		    ville: 'Ville du test',
+		    cp: '77880',
+		    mail: 'Steeve@apple.com'
+			 }
+		    	];
+	
+		self.nbcontact = function () {
+		    var compt = 0;
+		    angular.forEach(self.clients, function(client, key) {
+				compt = compt + 1;
+		    });
+		    return compt;
+		};
+		
+		
+		
 		}
 ]);
 
 angular.module("AppGestiondesContacts").directive('myClient', function() {
     return{
-    	templateUrl: 'my-client.html'
-        /*restrict:'EA',
-        scope:{client:"=personne",onClick:"&ngClick"},
-        templateUrl: function(elem, attr){
-            return 'client-'+attr.type+'.html';
-        }*/
+    	restrict: 'AE',
+    	 templateUrl: function(elem, attr){
+             return 'client-'+attr.type+'.html';
+    	 }
     }
 });
 
