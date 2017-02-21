0c60609db2fd157e20bd15dd664804f7d828407e
HEBERT Matthias TD1 <21405372@etu.unicaen.fr>
2017-02-21 15:22:06 +0100

    TD2_GestiondesContacts bouton suppression
    -Bouton de suppression effectif
    -Annulation des suppressions effective
    -Ajout d'un filtre sur les contacts pour ne pas afficher les contacts
    supprimer

--- TD2_GestiondesContacts/js/contactsController.js
+++ TD2_GestiondesContacts/js/contactsController.js
@@ -2,6 +2,7 @@ angular.module("AppGestiondesContacts").
 controller("contactsController", [ function(){
 	
 	var self = this;
+    self.afficherboutonannulersuppression = false;
 	
 	self.clients =
 			[
@@ -11,7 +12,8 @@ controller("contactsController", [ function(){
 		    adresse: '20 rue du test',
 		    ville: 'Ville du test',
 		    cp: '77880',
-		    mail: 'johnsmith@hotmail.fr'
+		    mail: 'johnsmith@hotmail.fr',
+                 deleted:false
 			 },
 			 {
 		    nom: 'GATES',
@@ -19,7 +21,8 @@ controller("contactsController", [ function(){
 		    adresse: '21 rue du test',
 		    ville: 'Ville du test',
 		    cp: '77880',
-		    mail: 'bill@microsoft.com'
+		    mail: 'bill@microsoft.com',
+                 deleted:false
 			 },
 			 {
 		    nom: 'JOBS',
@@ -27,22 +30,50 @@ controller("contactsController", [ function(){
 		    adresse: '20 rue du test',
 		    ville: 'Ville du test',
 		    cp: '77880',
-		    mail: 'Steeve@apple.com'
+		    mail: 'Steeve@apple.com',
+                 deleted:false
 			 }
 		    	];
 	
-		self.nbcontact = function () {
+		self.nbcontact = function (val) {
 		    var compt = 0;
 		    angular.forEach(self.clients, function(client, key) {
-				compt = compt + 1;
+                if (client.deleted==val){
+                    compt = compt + 1;
+                };
 		    });
 		    return compt;
 		};
-		
-		
-		
+
+    self.restaurerContact = function () {
+        angular.forEach(self.clients, function(client, key) {
+            if (client.deleted==true){
+                client.deleted=false;
+            };
+        });
+        self.afficherboutonannulersuppression = false;
+    };
+
+    self.supprimerContact = function (client) {
+        indice = self.clients.indexOf(client);
+        self.clients[indice].deleted=true;
+        self.afficherboutonannulersuppression = true;
+    };
+
 		}
 ]);
+
+angular.module("AppGestiondesContacts").filter('noDeleted', function(){
+    return function(input, symbol){
+    	self.tab=[];
+        angular.forEach(input, function(contact, key) {
+            if (contact.deleted==false){
+                self.tab.push(contact);
+            };
+        });
+        return tab;
+    };
+});
 
 angular.module("AppGestiondesContacts").directive('myClient', function() {
     return{
