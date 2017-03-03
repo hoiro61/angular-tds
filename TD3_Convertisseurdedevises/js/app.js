3125c05f78e5529aea4752d14f0e78c7cd5b70fa
hoiro <21405372@etu.unicaen>
2017-03-03 10:42:15 +0100

    TD3_Convertisseurdedevises conversion et swap effectifs

--- TD3_Convertisseurdedevises/js/app.js
+++ TD3_Convertisseurdedevises/js/app.js
@@ -1,1 +1,8 @@
-angular.module("AppConvertisseurdedevises", []);
+angular.module("AppConvertisseurdedevises", [])
+.config(['$sceDelegateProvider', function(sceDelegateProvider) {
+	  // We must whitelist the JSONP endpoint that we are using to show that we trust it
+	  sceDelegateProvider.resourceUrlWhitelist([
+	    'self',
+	    'https://free.currencyconverterapi.com/**'
+	  ]);
+	}]);
--- TD3_Convertisseurdedevises/js/devisesController.js
+++ TD3_Convertisseurdedevises/js/devisesController.js
@@ -2,8 +2,11 @@ angular.module("AppConvertisseurdedevises").
 controller("devisesController", ['$http', function(http) {
 	
 	var self = this;
-	self.selected="";
+	self.from="";
+	self.to="";
+	self.what="";
 	self.currencies={};
+	self.result=0;
 	
 	
 	http.get('app/data/currencymap.json').
@@ -13,6 +16,19 @@ controller("devisesController", ['$http', function(http) {
     function(response) {
         console.log("Erreur avec le statut Http : "+response.status);
     });
+	
+	self.getResult = function () {
+		http.jsonp('https://free.currencyconverterapi.com/api/v3/convert?compact=y&q='+self.from.code+'_'+self.to.code, {jsonpCallbackParam: 'callback'})
+		.then(function(response) {
+		        self.result=response.data[self.from.code+'_'+self.to.code].val;
+		});
+    };
+    
+    self.swap = function () {
+    	self.temp = self.from;
+		self.from = self.to;
+		self.to = self.temp;
+    };
 	
 		}
 ]);
